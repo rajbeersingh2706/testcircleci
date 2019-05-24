42a3dabcefa93bcd0833aa7b30d40b227cd672b2
rajbeersingh2706 <you@example.com>
2019-05-24 12:20:32 +0530

    modified

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 aws configure set default.region $AWS_REGION
 
 # Login to ECR
-$(aws ecr get-login --no-include-email --region eu-east-2)
+$(aws ecr get-login --no-include-email --region us-east-2)
 
 docker build -t 873930443481.dkr.ecr.us-east-2.amazonaws.com/labor-repo:latest -t 873930443481.dkr.ecr.us-east-2.amazonaws.com/labor-repo:$commit .
 docker push 873930443481.dkr.ecr.us-east-2.amazonaws.com/labor-repo:latest
