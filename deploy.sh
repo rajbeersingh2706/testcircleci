236c242220aaa51ef836dfb77c38c844c9564576
rajbeersingh2706 <you@example.com>
2019-05-24 13:36:56 +0530

    modifed

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@
 sudo docker stop $(sudo docker ps -aq)
 sudo docker rm $(sudo docker ps -aq)
 sudo docker rmi $(sudo docker image ls -aq)
+
+##
 sudo docker pull 873930443481.dkr.ecr.us-east-2.amazonaws.com/labor-repo:latest 
 #sudo docker run -it -d -p 443:443 873930443481.dkr.ecr.us-east-2.amazonaws.com/labor-repo:$commit 
