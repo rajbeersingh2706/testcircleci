d5972a9974d63ad394c65d99ea7eec6d8bcad54a
rajbeersingh2706 <you@example.com>
2019-05-24 13:00:40 +0530

    modified

--- push_to_ecr.sh
+++ push_to_ecr.sh
@@ -11,7 +11,6 @@ unzip awscli-bundle.zip
 sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
 # Configure AWS CLI
-aws s3 ls
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 aws configure set default.region $AWS_REGION
