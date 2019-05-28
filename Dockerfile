60fc97bf32c35b9b2efc1bd8a58d5177835628e8
rajbeersingh2706 <you@example.com>
2019-05-28 15:57:13 +0530

    modified

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,5 @@ COPY salido-labor.jar salido-labor.jar
 RUN pwd
 RUN ls -la
 
-
 #ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-Ddebug", "-jar", "-Dspring.profiles.active=prod", "/u01/labor/app/salido-labor.jar"]
 ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-Ddebug", "-jar", "/u01/labor/app/salido-labor.jar"]
--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 commit=$1
-echo "Here>> in Circle CI",$commit
-
-#sudo docker stop $(sudo docker ps -aq)
-#sudo docker rm $(sudo docker ps -aq)
-#docker kill $(docker ps -aq)
-#sudo docker rmi $(sudo docker image ls -aq)
-#sudo $(aws ecr get-login --no-include-email --region us-east-2)
-#sudo docker pull 873930443481.dkr.ecr.us-east-2.amazonaws.com/labor-repo:latest 
-#sudo docker run -it -d -p 443:443 873930443481.dkr.ecr.us-east-2.amazonaws.com/labor-repo:$commit
 cd /home/ubuntu/labor
+$(aws ecr get-login --no-include-email --region us-east-2)
 
-#sudo docker stack deploy -c docker-service.yml labor_service
-sudo docker pull 873930443481.dkr.ecr.us-east-2.amazonaws.com/labor-repo:$commit
-sudo docker service update --image 873930443481.dkr.ecr.us-east-2.amazonaws.com/labor-repo:$commit labor_service_labor
-sudo docker rmi $(sudo docker image ls -aq)
+if [ ! "$(sudo docker service ls | grep labor_service)" ]
+then
+ echo ">>IF >>>"
+ sudo docker stack deploy -c docker-service.yml labor_service
+else
+ echo "ELSE"
+ sudo docker pull 873930443481.dkr.ecr.us-east-2.amazonaws.com/labor-repo:$commit
+ sudo docker service update --image 873930443481.dkr.ecr.us-east-2.amazonaws.com/labor-repo:$commit labor_service_labor
+ #sudo docker rmi $(sudo docker image ls -aq)
+fi
+
