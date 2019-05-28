7109d070a73bfb62ccdf89c77f123363d7797f1b
rajbeersingh2706 <you@example.com>
2019-05-28 13:20:42 +0530

    modified

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,6 @@
 #sudo docker run -it -d -p 443:443 873930443481.dkr.ecr.us-east-2.amazonaws.com/labor-repo:$commit
 cd /home/ubuntu/labor
 #sudo docker rmi $(sudo docker image ls -aq)
-sudo docker stack deploy -c docker-service.yml labor_service
+#sudo docker stack deploy -c docker-service.yml labor_service
+sudo docker service update --image 873930443481.dkr.ecr.us-east-2.amazonaws.com/labor-repo:latest
+
