cc5854b4b2083df3d8b7b93d4cf90ce41ffca0a6
rajbeersingh2706 <you@example.com>
2019-05-28 18:39:10 +0530

    modified

--- src/main/java/com/salido/labor/controller/LaborCostController.java
+++
@@ -1,9 +0,0 @@
-package com.salido.labor.controller;
-
-import com.salido.labor.dto.LaborCostRequestDTO;
-
-public interface LaborCostController {
-
-    public String calculateTimecardCost(LaborCostRequestDTO ruleProcessorDTO);
-
-}
--- src/main/java/com/salido/labor/logic/RuleReProcessorLogic.java
+++
@@ -1,7 +0,0 @@
-package com.salido.labor.logic;
-
-public interface RuleReProcessorLogic {
-
-	void processTimeCards(final String token);
-
-}
