81aff5231af795fb0da4dac75f056531942c03fc
rajbeersingh2706 <you@example.com>
2019-05-28 16:22:00 +0530

    modified

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ sudo $(aws ecr get-login --no-include-email --region us-east-2)
 if [ ! "$(sudo docker service ls | grep labor_service)" ]
 then
  echo ">>IF >>>"
+ sudo docker pull 873930443481.dkr.ecr.us-east-2.amazonaws.com/labor-repo:latest
  sudo docker stack deploy -c docker-service.yml labor_service
 else
  echo "ELSE"
@@ -14,4 +15,5 @@ else
  sudo docker service update --image 873930443481.dkr.ecr.us-east-2.amazonaws.com/labor-repo:$commit labor_service_labor
  #sudo docker rmi $(sudo docker image ls -aq)
 fi
+
 
