8adab992723d56ea8ede4b7aa26b9306c285685e
rajbeersingh2706 <you@example.com>
2019-05-28 13:07:12 +0530

    modified

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,6 @@
 #sudo $(aws ecr get-login --no-include-email --region us-east-2)
 #sudo docker pull 873930443481.dkr.ecr.us-east-2.amazonaws.com/labor-repo:latest 
 #sudo docker run -it -d -p 443:443 873930443481.dkr.ecr.us-east-2.amazonaws.com/labor-repo:$commit
+cd /home/ubuntu/labor
 sudo docker rmi $(sudo docker image ls -aq)
 sudo docker stack deploy -c docker-service.yml labor_service
